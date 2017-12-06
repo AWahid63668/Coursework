594f1d6400309d25730657306ead2bfe56a597b0
Abdul <awahid.63668@farnborough.ac.uk>
2017-12-06 12:24:47 +0000

    6/12/17-Type 2

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToolBar;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import static javafx.scene.layout.Region.layoutInArea;
-
+import javax.swing.event.TreeModelEvent;
 
 public class Main extends Application {
+
+    private TreeModelEvent root;
+    private ListView ListView;
+
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -35,7 +35,8 @@ public class Main extends Application {
 
         //SEARCH BAR
 
-        TextField searchbar = new TextField("Search album, song or album");
+        TextField searchbar = new TextField();
+        searchbar.setPromptText("Search album, song or album");
         searchbar.setPrefSize(100, 20);
         topBorderPane.setCenter(searchbar);
 
@@ -46,18 +47,111 @@ public class Main extends Application {
         topBorderPane.setRight(logoutButton);
 
         // LEFT OF MAIN BORDER PANE
-
         VBox leftHandVBox = new VBox();
+
+        BorderPane LeftBorderPane = new BorderPane();
+        LeftBorderPane.setPadding(new Insets(40));
+        LeftBorderPane.setStyle("-fx-background-color: blue");
+        outsideBorderPane.setLeft(LeftBorderPane);
+
+        Label subheading = new Label( "Playlists");
+        subheading.setFont(Font.font("Arial", FontWeight.BOLD, 30));
+        leftHandVBox.getChildren().add(subheading);
+
+
         leftHandVBox.setPadding(new Insets(10));
         leftHandVBox.setSpacing(8);
+        leftHandVBox.setStyle("-fx-background-color:purple");
 
-        Text title = new Text("Playlists");
-        title.setFont(Font.font("Arial", FontWeight.BOLD, 14));
-        leftHandVBox.getChildren().add(title);
+        //leftHandVBox = new VBox(10);
+
+        Button[] myButtons;
+        myButtons = new Button[6];
+
+
+        myButtons[0] = new Button("Playlist 1");
+        myButtons[0].setPrefSize(200, 10);
+
+        myButtons[1] = new Button("Playlist 2");
+        myButtons[1].setPrefSize(200, 10);
+
+        myButtons[2] = new Button("Playlist 3");
+        myButtons[2].setPrefSize(200, 10);
+
+        myButtons[3] = new Button("Create A New Playlist");
+        myButtons[3].setPrefSize(200, 10);
+
+        myButtons[4] = new Button("Saved Music");
+        myButtons[4].setPrefSize(200, 10);
+
+        myButtons[5] = new Button("Favourite Songs");
+        myButtons[5].setPrefSize(200, 10);
+
+
+        leftHandVBox.getChildren().addAll(myButtons);
 
         outsideBorderPane.setLeft(leftHandVBox);
 
+        // RIGHT OF MAIN BORDER PANE
+
+        VBox rightHandVBox = new VBox();
+        rightHandVBox.setPadding(new Insets(10));
+        rightHandVBox.setSpacing(8);
+        rightHandVBox.setStyle("-fx-background-color: Pink;");
+
+        subheading = new Label("Similar Songs");
+        subheading.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+        rightHandVBox.getChildren().add(subheading);
+
+        //boxOfButtons = new VBox(10);
+
+        myButtons = new Button[5];
+
+        myButtons[0] = new Button("Similar Song 1");
+        myButtons[0].setPrefSize(200, 10);
+
+        myButtons[1] = new Button("Similar Song 2");
+        myButtons[1].setPrefSize(200, 10);
+
+        myButtons[2] = new Button("Similar Song 3");
+        myButtons[2].setPrefSize(200, 10);
+
+        myButtons[3] = new Button("Settings");
+        myButtons[3].setPrefSize(200, 10);
+
+        myButtons[4] = new Button("Languages");
+        myButtons[4].setPrefSize(200, 10);
+
+        //boxOfButtons.getChildren().addAll(myButtons);
+
+        //rightHandVBox.getChildren().add(boxOfButtons);
+
+        outsideBorderPane.setRight(rightHandVBox);
+
+        //BorderPane within Centre
+        BorderPane centreBorderPane = new BorderPane();
+        centreBorderPane.setPadding(new Insets(40));
+        centreBorderPane.setStyle("-fx-background-color: yellow;");
+        outsideBorderPane.setCenter(centreBorderPane);
+
+        subheading = new Label("PLAYLIST 1");
+        subheading.setFont(Font.font("Arial", FontWeight.BOLD, 30));
+        centreBorderPane.setTop(subheading);
+
+        ListView = new ListView();
+        ListView.getItems().addAll("Pain - Jimmy Eat World", "Obstacle 1 - Interpol ", "Obstacle 1 - Interpol ", "Obstacle 1 - Interpol ", "Obstacle 1 - Interpol ");
+        ListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        centreBorderPane.setCenter(ListView);
+
+
+        BorderPane BottomBorderPane = new BorderPane();
+        BottomBorderPane.setPadding(new Insets(40));
+        BottomBorderPane.setStyle("-fx-background-color: orange;");
+        outsideBorderPane.setBottom(BottomBorderPane);
+
+
     }
+
 
     public static void main(String[] args) {
         launch(args);
